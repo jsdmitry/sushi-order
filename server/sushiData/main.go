a9d382bdfa0b56ab30b5336b181ddd086315424a
CORP\semenov <dmitry.semenov@devexpress.com>
2018-07-22 15:14:29 +0300

    get categories as JSON

--- server/sushiData/main.go
+++ server/sushiData/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/jsdmitry/sushi-order/server/sql"
 )
 
@@ -10,7 +11,15 @@ const connectionString = "root:12qwesdf@/sushi_order"
 
 func main() {
 	sqlProvider := sql.SQLDataProvider{ConnectionString: connectionString}
-	for _, c := range sqlProvider.GetCategories() {
-		fmt.Println(c)
+	sqlProvider.ConnectToDB()
+
+	router := gin.Default()
+	group := router.Group("/sushi-data")
+	{
+		group.GET("categories/", func(context *gin.Context) {
+			categories := sqlProvider.GetCategories()
+			context.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": categories})
+		})
 	}
+	router.Run()
 }
