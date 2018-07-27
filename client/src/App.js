6da6c51d99302ac9dc6609edc319deacde0975b8
CORP\semenov <dmitry.semenov@devexpress.com>
2018-07-27 11:11:47 +0300

    add main layout component with paersistent drawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import MainLayout from './components/mainLayout';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputType: "string" };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MainLayout/>
     );
   }
 }
---
+++ client/src/components/mainLayout.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import PersistentDrawer from "./persistentDrawer"
+
+
+class MainLayout extends Component {
+  render(){
+    return (
+      <PersistentDrawer/>
+    );
+  }
+}
+
+export default MainLayout;
