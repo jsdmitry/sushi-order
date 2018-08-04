225445a7b102ff3a9e6c45540e60e5dc6c2b8c2d
CORP\semenov <dmitry.semenov@devexpress.com>
2018-08-04 09:31:07 +0300

    fix CORS

--- server/sushiData/main.go
+++ server/sushiData/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jsdmitry/sushi-order/server/sql"
@@ -11,11 +14,30 @@ import (
 
 const connectionString = "root:12qwesdf@/sushi_order"
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "false")
+
+		if c.Request.Method == "OPTIONS" {
+			fmt.Println("OPTIONS")
+			c.AbortWithStatus(200)
+		} else {
+			c.Next()
+		}
+	}
+}
+
 func main() {
 	sqlProvider := sql.SQLDataProvider{ConnectionString: connectionString}
 	sqlProvider.ConnectToDB()
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	group := router.Group("/sushi-data")
 	{
 		group.GET("categories/", func(context *gin.Context) {
@@ -32,5 +54,6 @@ func main() {
 			context.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": menu})
 		})
 	}
+
 	router.Run()
 }
